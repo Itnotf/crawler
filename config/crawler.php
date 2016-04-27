28f9cfebf31304f0383803d0dbc0a048910776a0
xuchu.xing <1096364408@qq.com>
2016-04-27 20:33:43 +0800

    update config

---
+++ config/crawler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Date: 2016/4/27
+ * Time: 17:31
+ */
+
+return [
+    'mysql' => [
+        'host'     => '127.0.0.1',
+        'username' => 'root',
+        'password' => 'root',
+        'charset'  => 'utf8',
+        'prefix'   => '',
+    ],
+];
